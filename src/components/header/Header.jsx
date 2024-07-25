6efe1b62162bb2eb8fb3ca269d6fe5dbaef7572b
ManonEche <manon.eche@laposte.net>
2024-07-25 16:43:21 +0200

    feat: :art: Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="px-6 py-4">
-      <Image src="/logo.webp" width={500} height={500} alt="Logo"/>
+    <header className="px-6 py-4 flex items-center gap-6">
+      <Image src="/logo.webp" width={500} height={500} alt="Logo" className="w-16" />
+      <h1 className="text-3xl">Tic Tac Toe</h1>
     </header>
   )
 }
